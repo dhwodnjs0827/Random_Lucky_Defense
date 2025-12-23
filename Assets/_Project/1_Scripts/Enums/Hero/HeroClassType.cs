3c11211ab3915381e304aa4f44e4dad020fa511c
dhwodnjs <dhwodnjs0827@naver.com>
2025-12-23 18:09:18 +0900

    feat: 영웅 enum 추가

---
+++ Assets/_Project/1_Scripts/Enums/Hero/HeroClassType.cs
@@ -0,0 +1,9 @@
+/// <summary>
+/// 영웅 클래스 종류
+/// </summary>
+public enum HeroClassType
+{
+    Magician, // 마법사
+    Archer, // 궁수
+    Warrior, // 전사
+}
---
+++ Assets/_Project/1_Scripts/Enums/Hero/HeroRankType.cs
@@ -0,0 +1,9 @@
+/// <summary>
+/// 영웅 랭크 종류
+/// </summary>
+public enum HeroRankType
+{
+    B,
+    A,
+    S
+}
