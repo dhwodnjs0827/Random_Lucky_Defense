6939a2a3b17efa766a33deb0fd260f9dd50c237d
dhwodnjs <dhwodnjs0827@naver.com>
2025-12-26 14:07:41 +0900

    chore: 궁수 리소스 추가

---
+++ Assets/_Project/1_Scripts/Core/Hero/ArcherHero.cs
@@ -0,0 +1,22 @@
+using Generated;
+
+public class ArcherHero : BaseHero
+{
+    public override HeroClassType ClassType => HeroClassType.Archer;
+
+    public override void Initialize(HeroDataSO data)
+    {
+    }
+
+    public override void Attack(BaseEnemy target)
+    {
+    }
+
+    public override void OnGet()
+    {
+    }
+
+    public override void OnRelease()
+    {
+    }
+}
