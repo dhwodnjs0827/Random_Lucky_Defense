ac5579523961009928450d456c095ccd2e4c8e43
dhwodnjs <dhwodnjs0827@naver.com>
2025-12-29 11:18:16 +0900

    feat: Enemy Flip

--- Assets/_Project/1_Scripts/Core/Enemy/BaseEnemy.cs
+++ Assets/_Project/1_Scripts/Core/Enemy/BaseEnemy.cs
@@ -5,15 +5,24 @@ using UnityEngine.Splines;
 /// 모든 Enemy의 부모 클래스
 /// </summary>
 [RequireComponent(typeof(SplineAnimate))]
-public abstract class BaseEnemy : MonoBehaviour
+public abstract class BaseEnemy : MonoBehaviour, IPoolable
 {
     private static readonly int EnemyMoveAnimParam = Animator.StringToHash("1_Move");
+    
     [SerializeField] private SplineAnimate splineAnimate;
     [SerializeField] private Animator animator;
 
-    protected virtual void Awake()
+    private Vector3 previousPosition;
+    private const float FlipThreshold = 0.01f;
+
+    private void Awake()
     {
         SetSplineAnimateComponent();
+    }
+    
+    private void Update()
+    {
+        UpdateFlip();
     }
 
     /// <summary>
@@ -50,5 +59,33 @@ public abstract class BaseEnemy : MonoBehaviour
         splineAnimate.MaxSpeed = 5f;
         splineAnimate.Loop = SplineAnimate.LoopMode.Loop;
         splineAnimate.PlayOnAwake = false;
+    }
+
+    private void UpdateFlip()
+    {
+        var currentPosition = transform.position;
+        var directionX = currentPosition.x - previousPosition.x;
+
+        if (directionX > FlipThreshold)
+        {
+            transform.rotation = Quaternion.Euler(0f, 180f, 0f);
+        }
+        else if (directionX < -FlipThreshold)
+        {
+            transform.rotation = Quaternion.Euler(0f, 0f, 0f);
+        }
+
+        previousPosition = currentPosition;
+    }
+
+    public void OnGet()
+    {
+        splineAnimate.Restart(false);
+        previousPosition = transform.position;
+    }
+
+    public void OnRelease()
+    {
+        splineAnimate.Pause();
     }
 }
