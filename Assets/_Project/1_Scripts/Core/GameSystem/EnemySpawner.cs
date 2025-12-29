8be901c4cc9f2a5936d7a7c4f9299f5f0c74b02e
dhwodnjs <dhwodnjs0827@naver.com>
2025-12-29 10:25:36 +0900

    feat: 적 소환

--- Assets/_Project/1_Scripts/Core/GameSystem/EnemySpawner.cs
+++ Assets/_Project/1_Scripts/Core/GameSystem/EnemySpawner.cs
@@ -4,7 +4,17 @@ using UnityEngine.Splines;
 public class EnemySpawner : MonoBehaviour
 {
     [SerializeField] private SplineContainer splineContainer;
+    [SerializeField] private BaseEnemy normalEnemy;
+    [SerializeField] private BaseEnemy bossEnemy;
+    
     private ObjectPoolManager objectPoolManager;
+
+    private Vector3 spawnPoint;
+
+    private void Awake()
+    {
+        Initialize();
+    }
 
     private void Start()
     {
@@ -13,9 +23,20 @@ public class EnemySpawner : MonoBehaviour
 
     public void Spawn()
     {
-        // var enemy = objectPoolManager.Get();
-        // enemy.transform.SetParent(transform);
-        
+        var enemy = objectPoolManager.Get(normalEnemy);
+        enemy.transform.SetParent(transform);
+        enemy.transform.position = spawnPoint;
         CDebug.Log("[EnemySpawner] 몬스터 스폰!");
+    }
+
+    private void Initialize()
+    {
+        if (splineContainer == null)
+        {
+            CDebug.LogError("[EnemySpawner] SplineContainer가 없습니다!");
+            return;
+        }
+
+        spawnPoint = splineContainer.transform.TransformPoint(splineContainer.Spline[0].Position);
     }
 }
