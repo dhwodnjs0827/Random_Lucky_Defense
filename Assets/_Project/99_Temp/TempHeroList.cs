e6080b1fdfd9ce62dd4adffe4bc4c2eabe056d06
dhwodnjs <dhwodnjs0827@naver.com>
2025-12-29 17:18:39 +0900

    chore: 임시 선택 영웅 데이터 추가

---
+++ Assets/_Project/99_Temp/TempHeroList.cs
@@ -0,0 +1,38 @@
+using Generated;
+using UnityEngine;
+
+public class TempHeroList : MonoBehaviour
+{
+    [Header("선택한 마법사 데이터")]
+    [SerializeField] private HeroDataSO normalMagician;
+    [SerializeField] private HeroDataSO superiorMagician;
+    [SerializeField] private HeroDataSO rareMagician;
+    [SerializeField] private HeroDataSO ancientMagician;
+    [SerializeField] private HeroDataSO relicMagician;
+    [SerializeField] private HeroDataSO legendMagician;
+    [SerializeField] private HeroDataSO epicMagician;
+    [SerializeField] private HeroDataSO mythMagician;
+    [SerializeField] private HeroDataSO godMagician;
+    
+    [Header("선택한 궁수 데이터")]
+    [SerializeField] private HeroDataSO normalArcher;
+    [SerializeField] private HeroDataSO superiorArcher;
+    [SerializeField] private HeroDataSO rareArcher;
+    [SerializeField] private HeroDataSO ancientArcher;
+    [SerializeField] private HeroDataSO relicArcher;
+    [SerializeField] private HeroDataSO legendArcher;
+    [SerializeField] private HeroDataSO epicArcher;
+    [SerializeField] private HeroDataSO mythArcher;
+    [SerializeField] private HeroDataSO godArcher;
+    
+    [Header("선택한 전사 데이터")]
+    [SerializeField] private HeroDataSO normalWarrior;
+    [SerializeField] private HeroDataSO superiorWarrior;
+    [SerializeField] private HeroDataSO rareWarrior;
+    [SerializeField] private HeroDataSO ancientWarrior;
+    [SerializeField] private HeroDataSO relicWarrior;
+    [SerializeField] private HeroDataSO legendWarrior;
+    [SerializeField] private HeroDataSO epicWarrior;
+    [SerializeField] private HeroDataSO mythWarrior;
+    [SerializeField] private HeroDataSO godWarrior;
+}
