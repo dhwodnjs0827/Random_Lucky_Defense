74a53fa4b3874ae36dd8d27da474f4980ef27029
dhwodnjs <dhwodnjs0827@naver.com>
2026-01-07 17:40:07 +0900

    chore: HeroManageUI 구성

---
+++ Assets/_Project/1_Scripts/UI/Component/HeroViewComponent.cs
@@ -0,0 +1,30 @@
+using Generated;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HeroViewComponent : MonoBehaviour
+{
+    [Header("UI Components")]
+    [SerializeField] private Button heroInfoButton;
+    [Space]
+    [SerializeField] private Image heroRankImage;
+    [SerializeField] private TextMeshProUGUI heroGradeText;
+    [SerializeField] private Image heroImage;
+    [SerializeField] private TextMeshProUGUI heroRankText;
+    [Space]
+    [SerializeField] private Image isSelectedImage;
+    [Space]
+    [SerializeField] private TextMeshProUGUI heroLevelText;
+    [Space]
+    [SerializeField] private Slider heroRequiredLevelSlider;
+    [SerializeField] private TextMeshProUGUI heroRequiredLevelText;
+
+    /// <summary>
+    /// 영웅 정보에 맞게 UI 요소들 초기화
+    /// </summary>
+    public void InitializeHeroViewUIComponent(HeroDataSO heroData)
+    {
+        
+    }
+}
