3f64198b0c6098e28488da39914c0211ee1ecd3b
dhwodnjs <dhwodnjs0827@naver.com>
2026-01-12 15:53:16 +0900

    chore: 임시 저장 데이터 표시

--- Assets/_Project/1_Scripts/UI/MainLobbyUI.cs
+++ Assets/_Project/1_Scripts/UI/MainLobbyUI.cs
@@ -1,13 +1,23 @@
 using Cysharp.Threading.Tasks;
+using TMPro;
 using UnityEngine;
 
 public class MainLobbyUI : BaseUI
 {
+    //TODO: 임시
+    [SerializeField] private TextMeshProUGUI userNameText;
+    [SerializeField] private TextMeshProUGUI goldText;
+    [SerializeField] private TextMeshProUGUI levelText;
+    
     [SerializeField] private GameStartButtonComponent gameStartButton;
     [SerializeField] private LobbyBottomButtonGroups bottomButtonGroups;
-    
+
     protected override void Opened(params object[] args)
     {
+        //TODO: 임시
+        userNameText.text = PlayerDataManager.Instance.ProfileSaveData.PlayerName;
+        goldText.text = PlayerDataManager.Instance.CurrencySaveData.Gold.ToString();
+        levelText.text = PlayerDataManager.Instance.ProfileSaveData.Level.ToString();
     }
 
     protected override void Closed(params object[] args)
