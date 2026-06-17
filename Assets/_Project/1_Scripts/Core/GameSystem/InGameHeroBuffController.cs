c47f8cc6fc2709d7a8b2fcd11349a3a7000cb861
Jaewon <dhwodnjs0827@naver.com>
2026-06-17 16:37:41 +0900

    feat: 영웅 스탯 관련 재능 효과 적용 베이스 코드 추가

--- Assets/_Project/1_Scripts/Core/GameSystem/InGameHeroBuffController.cs
+++ Assets/_Project/1_Scripts/Core/GameSystem/InGameHeroBuffController.cs
@@ -52,20 +52,38 @@ public class InGameHeroBuffController : IEventListener, IAbilityEffect
     {
         abilityEffectFactory.RegisterAbilityEffectHandler(AbilityEffectType.IncreaseCriticalRate, this);
         abilityEffectFactory.RegisterAbilityEffectHandler(AbilityEffectType.IncreaseCriticalDamage, this);
+        abilityEffectFactory.RegisterAbilityEffectHandler(AbilityEffectType.IncreaseDamage, this);
+        abilityEffectFactory.RegisterAbilityEffectHandler(AbilityEffectType.IncreaseAttackRange, this);
+        abilityEffectFactory.RegisterAbilityEffectHandler(AbilityEffectType.IncreaseSplashRange, this);
+        abilityEffectFactory.RegisterAbilityEffectHandler(AbilityEffectType.IncreasePenetratingPower, this);
+        abilityEffectFactory.RegisterAbilityEffectHandler(AbilityEffectType.MagicianIncreaseAttackPower, this);
         abilityEffectFactory.RegisterAbilityEffectHandler(AbilityEffectType.MagicianIncreaseMoveSpeed, this);
+        abilityEffectFactory.RegisterAbilityEffectHandler(AbilityEffectType.MagicianSummonRedDragon, this);
+        abilityEffectFactory.RegisterAbilityEffectHandler(AbilityEffectType.ArcherIncreaseAttackPower, this);
         abilityEffectFactory.RegisterAbilityEffectHandler(AbilityEffectType.ArcherIncreaseMoveSpeed, this);
         abilityEffectFactory.RegisterAbilityEffectHandler(AbilityEffectType.ArcherSummonAncientStatue, this);
+        abilityEffectFactory.RegisterAbilityEffectHandler(AbilityEffectType.KnightIncreaseAttackPower, this);
         abilityEffectFactory.RegisterAbilityEffectHandler(AbilityEffectType.KnightIncreaseMoveSpeed, this);
+        abilityEffectFactory.RegisterAbilityEffectHandler(AbilityEffectType.KnightLightning, this);
     }
 
     public void UnregisterAbilityEffect(AbilityEffectFactory abilityEffectFactory)
     {
         abilityEffectFactory.UnregisterAbilityEffectHandler(AbilityEffectType.IncreaseCriticalRate, this);
         abilityEffectFactory.UnregisterAbilityEffectHandler(AbilityEffectType.IncreaseCriticalDamage, this);
+        abilityEffectFactory.UnregisterAbilityEffectHandler(AbilityEffectType.IncreaseDamage, this);
+        abilityEffectFactory.UnregisterAbilityEffectHandler(AbilityEffectType.IncreaseAttackRange, this);
+        abilityEffectFactory.UnregisterAbilityEffectHandler(AbilityEffectType.IncreaseSplashRange, this);
+        abilityEffectFactory.UnregisterAbilityEffectHandler(AbilityEffectType.IncreasePenetratingPower, this);
+        abilityEffectFactory.UnregisterAbilityEffectHandler(AbilityEffectType.MagicianIncreaseAttackPower, this);
         abilityEffectFactory.UnregisterAbilityEffectHandler(AbilityEffectType.MagicianIncreaseMoveSpeed, this);
+        abilityEffectFactory.UnregisterAbilityEffectHandler(AbilityEffectType.MagicianSummonRedDragon, this);
+        abilityEffectFactory.UnregisterAbilityEffectHandler(AbilityEffectType.ArcherIncreaseAttackPower, this);
         abilityEffectFactory.UnregisterAbilityEffectHandler(AbilityEffectType.ArcherIncreaseMoveSpeed, this);
         abilityEffectFactory.UnregisterAbilityEffectHandler(AbilityEffectType.ArcherSummonAncientStatue, this);
+        abilityEffectFactory.UnregisterAbilityEffectHandler(AbilityEffectType.KnightIncreaseAttackPower, this);
         abilityEffectFactory.UnregisterAbilityEffectHandler(AbilityEffectType.KnightIncreaseMoveSpeed, this);
+        abilityEffectFactory.UnregisterAbilityEffectHandler(AbilityEffectType.KnightLightning, this);
     }
 
     public void ApplyAbilityEffect(AbilityContainer abilityContainer)
@@ -82,17 +100,44 @@ public class InGameHeroBuffController : IEventListener, IAbilityEffect
                 abilityEffectStats[HeroClassType.Archer].IncreaseCriticalDamage(abilityContainer.AbilityLevelData.value / 100f);
                 abilityEffectStats[HeroClassType.Knight].IncreaseCriticalDamage(abilityContainer.AbilityLevelData.value / 100f);
                 break;
+            case AbilityEffectType.IncreaseDamage:
+                CDebug.Log($"[InGameHeroBuffController] 피해량 {abilityContainer.AbilityLevelData.value}% 증가");
+                break;
+            case AbilityEffectType.IncreaseAttackRange:
+                CDebug.Log($"[InGameHeroBuffController] 모든 클래스 공격 범위 {abilityContainer.AbilityLevelData.value} 증가");
+                break;
+            case AbilityEffectType.IncreaseSplashRange:
+                CDebug.Log($"[InGameHeroBuffController] 스플래시 데미지 범위{abilityContainer.AbilityLevelData.value} 증가");
+                break;
+            case AbilityEffectType.IncreasePenetratingPower:
+                CDebug.Log($"[InGameHeroBuffController] 적 방어력 {abilityContainer.AbilityLevelData.value}% 무시함");
+                break;
+            case AbilityEffectType.MagicianIncreaseAttackPower:
+                CDebug.Log($"[InGameHeroBuffController] 마법사 기본 공격력의 {abilityContainer.AbilityLevelData.value}% 증가");
+                break;
             case AbilityEffectType.MagicianIncreaseMoveSpeed:
                 abilityEffectStats[HeroClassType.Magician].IncreaseMoveSpeedMultiplier(abilityContainer.AbilityLevelData.value / 100f);
+                break;
+            case AbilityEffectType.MagicianSummonRedDragon:
+                CDebug.Log($"[InGameHeroBuffController] 레드 드래곤 소환");
+                break;
+            case AbilityEffectType.ArcherIncreaseAttackPower:
+                CDebug.Log($"[InGameHeroBuffController] 궁수 기본 공격력의 {abilityContainer.AbilityLevelData.value}% 증가");
                 break;
             case AbilityEffectType.ArcherIncreaseMoveSpeed:
                 abilityEffectStats[HeroClassType.Archer].IncreaseMoveSpeedMultiplier(abilityContainer.AbilityLevelData.value / 100f);
                 break;
+            case AbilityEffectType.ArcherSummonAncientStatue:
+                CDebug.Log("[InGameHeroBuffController] 고대 석상 소환");
+                break;
+            case AbilityEffectType.KnightIncreaseAttackPower:
+                CDebug.Log($"[InGameHeroBuffController] 전사 기본 공격력의 {abilityContainer.AbilityLevelData.value}% 증가");
+                break;
             case AbilityEffectType.KnightIncreaseMoveSpeed:
                 abilityEffectStats[HeroClassType.Knight].IncreaseMoveSpeedMultiplier(abilityContainer.AbilityLevelData.value / 100f);
                 break;
-            case AbilityEffectType.ArcherSummonAncientStatue:
-                CDebug.Log("[ArcherCardEffectHandler] 고대 석상 소환]");
+            case AbilityEffectType.KnightLightning:
+                CDebug.Log("[InGameHeroBuffController] 번개 소환");
                 break;
             default:
                 throw new ArgumentOutOfRangeException();
