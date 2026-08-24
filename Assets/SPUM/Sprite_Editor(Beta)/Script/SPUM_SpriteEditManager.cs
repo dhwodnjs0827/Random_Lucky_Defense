9470a2120fd0f095afa130a05fcf048a602f303c
Jaewon <dhwodnjs0827@naver.com>
2026-08-24 15:19:17 +0900

    chore: 심볼 추가 및 프로젝트 설정 변경

--- Assets/SPUM/Sprite_Editor(Beta)/Script/SPUM_SpriteEditManager.cs
+++ Assets/SPUM/Sprite_Editor(Beta)/Script/SPUM_SpriteEditManager.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿#if UNITY_EDITOR
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
@@ -342,3 +343,4 @@ public class SPUM_SpriteEditManager : MonoBehaviour
 
     #endif
 }
+#endif
