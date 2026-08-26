495dcd86b11105724618f0eb6a2a83c32ef418af
Jaewon <dhwodnjs0827@naver.com>
2026-08-26 11:36:29 +0900

    chore: 어드레서블 관련 전처리기 제거

--- Assets/_Project/1_Scripts/Core/GameSystem/InGameHeroLevelUpController.cs
+++ Assets/_Project/1_Scripts/Core/GameSystem/InGameHeroLevelUpController.cs
@@ -15,12 +15,8 @@ public class InGameHeroLevelUpController : IEventListener
     private readonly Dictionary<HeroClassType, ReactiveProperty<int>> currentLevelDict = new(); // 클래스 별 현재 레벨
 
     private Action<HeroClassType> onLevelUp;
-
-#if ADDRESSABLE
+    
     private const string IN_GAME_HERO_LEVEL_UP_DATA_SO_PATH = "InGameHeroLevelUpData";
-#else
-    private const string IN_GAME_HERO_LEVEL_UP_DATA_SO_PATH = "Data/SO/InGameHeroLevelUpData";
-#endif
 
     public IDictionary<HeroClassType, Dictionary<int, ClassLevelUpData>> LevelUpDataDict => levelUpDataDict;
     public IDictionary<HeroClassType, ReactiveProperty<int>> CurrentLevelDict => currentLevelDict;
